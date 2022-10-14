54211b3a514d166abdea173530b23a33e8a1eaab
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-10-14 20:10:43 -0300

    feat: configure status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
+import { StatusBar } from "react-native";
 import Home from "./src/Screens/Home";
 
 export default function App() {
-  return <Home />
+  return (
+    <>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent 
+      />
+      <Home />
+    </>
+  )
 }
